07e7d5c3c1bd733adba29e96accd8b98215308d6
19rahulyadav38 <19rahulyadav38@gmail.com>
2021-11-28 20:18:56 +0530

    changes in iframe

--- src/FormInput.js
+++ src/FormInput.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+// import {useState} from "react"
 
 function FormInput() {
   //   const [name, setName] = useState("");
@@ -55,7 +56,7 @@ function FormInput() {
       
       <div className="md:mx-12 py-4 flex flex-col md:flex-row">
         <div className="rounded-2xl flex-1 ">
-             <iframe className="rounded-xl w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224022.47527582682!2d76.97804307134898!3d28.67908072379827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d047309fff32f%3A0xfc5606ed1b5d46c3!2sDelhi!5e0!3m2!1sen!2sin!4v1638104294668!5m2!1sen!2sin"  height="450"  allowfullscreen="" loading="lazy"></iframe>
+             <iframe title="myFrame" className="rounded-xl w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224022.47527582682!2d76.97804307134898!3d28.67908072379827!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d047309fff32f%3A0xfc5606ed1b5d46c3!2sDelhi!5e0!3m2!1sen!2sin!4v1638104294668!5m2!1sen!2sin"  height="450"  allowfullscreen="" loading="lazy"></iframe>
         </div>
 
         <div className="text-center flex-1 ">
